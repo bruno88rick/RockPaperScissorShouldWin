b0e0105e9b2c8c19817ac607c48c3c781b1a29c7
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-02-03 12:09:40 -0300

    Initial Commit

---
+++ RockPaperScissorShouldWin/RockPaperScissorShouldWinApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorShouldWinApp.swift
+//  RockPaperScissorShouldWin
+//
+//  Created by Bruno Oliveira on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorShouldWinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
