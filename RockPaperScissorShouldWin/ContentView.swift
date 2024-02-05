7a223b1161831bdd9732d3abd411606074d19a47
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-02-05 19:47:42 -0300

    Game logic completed

--- RockPaperScissorShouldWin/ContentView.swift
+++ RockPaperScissorShouldWin/ContentView.swift
@@ -22,6 +22,8 @@ struct ContentView: View {
     @State private var numberOfQuestion = 1
     @State private var showingGameOver = false
     @State private var showingResult = false
+    @State private var showingMatchButton = false
+    @State private var userChoiceIsCorrect = false
     @State private var updateMachineMovement = false
     @State private var score = 0
     
@@ -31,7 +33,9 @@ struct ContentView: View {
                 .font(.system(size: 22).bold())
                 .foregroundColor(.primary)
             Button {
-                
+                machineMoveChange()
+                updateMachineMovement = true
+                showingResult = false
             } label: {
                 Text(moves[machineMove])
                     .font(.system(size: 200))
@@ -42,23 +46,45 @@ struct ContentView: View {
                 .hiddenConditionally(isHidden: updateMachineMovement)
             Spacer()
             ZStack {
-                Text("Parabéns! Você Ganhou!")
+                Text(userChoiceIsCorrect ? "Parabéns Você acertou!" : "Ih... escolha errada!")
                     .font(.system(size: 30).bold())
                     .hiddenConditionally(isHidden: showingResult)
+                    .foregroundStyle(userChoiceIsCorrect ? .mint : .brown)
                 ProgressView()
                     .hiddenConditionally(isHidden: !showingResult)
             }
+            HStack {
+                Button {
+                    showingMatchButton = false
+                    machineMoveChange()
+                    shouldWin = Bool.random()
+                } label: {
+                    Text("Próxima Partida")
+                }
+                .frame(width: 200, height: 50)
+                .foregroundStyle(.white)
+                .font(.system(size:20).bold())
+                .background(.blue)
+                .clipShape(.rect(cornerRadius: 20, style: .circular))
+                .alert("GameOver", isPresented: $showingGameOver){
+                    Button("Reiniciar Jogo", action: resetGame)
+                } message: {
+                    Text("Sua pontuação foi de \(score) pontos!")
+                }
+            }
+            .hiddenConditionally(isHidden: showingMatchButton)
+            
             Spacer()
-            Spacer()
-            Text(shouldWin ? "Você precisa Ganhar! Escolha:": "Você precisa Perder! Escolha:")
-                .font(.system(size: 20).bold())
+            Text(shouldWin ? "Qual irá ganhar da I.A.?": "Qual irá perder p/ a I.A.?")
+                .font(.system(size: 25).bold())
                 .foregroundStyle(shouldWin ? .green : .red)
-            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 80) {
-                ForEach(moves, id: \.self) { number in
+            HStack(alignment: .center, spacing: 80) {
+                ForEach(0..<3) { number in
                     Button {
-                        
+                        gameLogic(userChoice: number)
+                        updateMachineMovement = false
                     } label: {
-                        Text(number)
+                        Text(moves[number])
                             .font(.system(size: 50))
                     }
                 }
@@ -68,6 +94,50 @@ struct ContentView: View {
                 .font(.system(size: 30).bold())
         }
         .padding()
+    }
+    
+    func gameLogic (userChoice: Int) {
+        let winnerMove = [1, 2, 0]
+        let didWin: Bool
+        
+        if shouldWin {
+            didWin = userChoice == winnerMove[machineMove]
+        } else {
+            didWin = winnerMove[userChoice] == machineMove
+        }
+        
+        if didWin {
+            userChoiceIsCorrect = true
+            showingResult = true
+            showingMatchButton = true
+            score += 1
+            numberOfQuestion += 1
+        } else {
+            userChoiceIsCorrect = false
+            showingResult = true
+            showingMatchButton = true
+            score -= 1
+            numberOfQuestion += 1
+        }
+        
+        if numberOfQuestion == 8 {
+            showingGameOver = true
+        }
+    }
+    
+    func machineMoveChange() {
+        machineMove = Int.random(in: 0..<3)
+    }
+    
+    func resetGame() {
+        shouldWin = Bool.random()
+        machineMove = Int.random(in: 0..<3)
+        numberOfQuestion = 1
+        showingGameOver = false
+        showingResult = false
+        showingMatchButton = false
+        updateMachineMovement = false
+        score = 0
     }
 }
 
